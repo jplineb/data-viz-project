170951fdcc93ed38f2a39802c79485f5037eb332
Shrayas <111436421+Shrayas555@users.noreply.github.com>
2024-12-03 15:16:00 -0500

    Update timeDistribution.js

--- scripts/timeDistribution.js
+++ scripts/timeDistribution.js
@@ -3,8 +3,8 @@ function createTimeDistribution(accidentsData, stateAbbr, title=null) {
     d3.select("#time-distribution-container").html("");
 
     const margin = { top: 75, right: 50, bottom: 50, left: 50 };
-    const width = 500 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+    const width = 434 - margin.left - margin.right;
+    const height = 434 - margin.top - margin.bottom;
     const innerRadius = 10;
     const outerRadius = Math.min(width, height) / 2;
 
